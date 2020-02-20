3d4589ab0c5d5ef99961992efcb05690913699ef
artclic <60551636+artclic@users.noreply.github.com>
2020-02-20 16:41:41 -0600

    441
    OTRA

--- SqlBaseCon/Form1.Designer.vb
+++ SqlBaseCon/Form1.Designer.vb
@@ -60,6 +60,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
         Me.ClientSize = New System.Drawing.Size(205, 262)
         Me.Controls.Add(Me.RadioButton1)
         Me.Controls.Add(Me.Button2)
